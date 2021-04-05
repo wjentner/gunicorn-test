69d406a06ee8b83521f930e6ade4ca60fe46d64d
Wolfgang Jentner <wolfgang.jentner@uni-konstanz.de>
2021-04-05 13:42:42 +0200

    fix typo

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,7 +4,7 @@ bind = "0.0.0.0:5000"
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = multiprocessing.cpu_count() * 2 + 1
 
-# doe not work with gunicorn v20.1.0 anymore
+# does not work with gunicorn v20.1.0 anymore, but works in v20.0.4
 print_config = True
 
 accesslog = '-'
